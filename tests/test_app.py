7045677b86e1de3149fa9356a4a2b05fa4d09d71
Basie12 <basazin.tilahun92@gmail.com>
2026-06-11 00:07:56 -0400

    Add src package init file

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,6 @@
-
 from src.app import app
+
 def test_home():
-    response = app.test_client().get('/')
+    response = app.test_client().get("/")
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == "Hello, MLOPS!"
+    assert response.data.decode("utf-8") == "Hello, MLOPS!"
